98ffdba115504ed4d3e816341087e2b37a78b8ba
Thomas Kölling <spamdoggi@gmail.com>
2016-04-09 14:12:06 +0200

    Importer reads out now a directory of tcx files and push them to the rest-api

--- importer/tcx/index.js
+++ importer/tcx/index.js
@@ -4,6 +4,7 @@
 const Promise = require('bluebird')
 const fs = Promise.promisifyAll(require('fs'))
 const request = Promise.promisify(require('request'))
+const ls = require('deep-readdir').deepReaddirSync
 
 require('co-mmander')
 const program = require('commander')
@@ -14,16 +15,18 @@ const readData = ( fileName => {
 })
 
 const postAction = function * (path) {
-  const content = yield readData('samples/2015-09-19_Running.tcx')
-  const requestPromises = deserialize(content).map(function (dto) {
-    return request({
-      method: 'POST',
-      uri: 'http://localhost:3000/api/v1/activity',
-      json: true,
-      body: dto
+  for (let filename of ls(path, {extension: 'tcx'})) {
+    const content = yield readData(filename)
+    const requestPromises = deserialize(content).map(function (dto) {
+      return request({
+        method: 'POST',
+        uri: 'http://localhost:3000/api/v1/activity',
+        json: true,
+        body: dto
+      })
     })
-  })
-  yield Promise.all(requestPromises)
+    yield Promise.all(requestPromises)
+  }
 }
 
 program
