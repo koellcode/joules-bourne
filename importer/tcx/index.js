f0fde8ee2bf0e4bfa0b1ba778b50efd451777182
Thomas Kölling <spamdoggi@gmail.com>
2016-04-09 13:41:45 +0200

    Improve readability a bit more by removing Promise coroutine

--- importer/tcx/index.js
+++ importer/tcx/index.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
 "use strict"
 
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
 const request = Promise.promisify(require('request'))
 
-const program = require('commander');
+require('co-mmander')
+const program = require('commander')
 const deserialize = require('./tcx-to-dto')
 
 const readData = ( fileName => {
   return fs.readFileAsync(fileName, 'utf8')
 })
 
-const postAction = Promise.coroutine(function * (path) {
+const postAction = function * (path) {
   const content = yield readData('samples/2015-09-19_Running.tcx')
-  deserialize(content).map(Promise.coroutine(function * (dto) {
-    yield request({
+  const requestPromises = deserialize(content).map(function (dto) {
+    return request({
       method: 'POST',
       uri: 'http://localhost:3000/api/v1/activity',
       json: true,
       body: dto
     })
-  }))
-})
+  })
+  yield Promise.all(requestPromises)
+}
 
 program
   .version('0.0.1')
