e911bb017410feefbc351eccb9798a56a39d69ca
Thomas Kölling <spamdoggi@gmail.com>
2016-05-01 22:15:29 +0200

    Emit the startDate of an activity as index key,
    this allows us to order by date, get date ranges and other stuff

--- server/lib/activity/views/amount.js
+++ server/lib/activity/views/amount.js
@@ -2,7 +2,7 @@
 module.exports = {
   map: function (doc) {
     if (doc.type === 'activity' && typeof doc.laps !== 'undefined' && doc.laps[0].StartTime) {
-      emit(doc._id, 1)
+      emit(doc.laps[0].StartTime, 1)
     }
   }
 }
