a2af3cc62cad18911645cd5e200726bc16243541
Thomas Kölling <spamdoggi@gmail.com>
2016-05-03 23:55:53 +0200

    return 0 when 0

--- server/lib/activity/service/amount.js
+++ server/lib/activity/service/amount.js
@@ -3,6 +3,7 @@
 module.exports = (db) => {
   return function * () {
     const result = yield db.query('activity/amount')
+    if (result.rows.length === 0) return 0
     return result.rows[0].value
   }
 }
