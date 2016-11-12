f2b06617ec031f1cb5929c4c9fddef9860268c74
Thomas Kölling <spamdoggi@gmail.com>
2016-11-12 17:42:19 +0100

    improve readability a bit

--- server/lib/activity/index.js
+++ server/lib/activity/index.js
@@ -26,71 +26,77 @@ async function getActivityList (ctx, next) {
   }
 }
 
-module.exports = {
-  postActivity: async (ctx, next) => {
-    if (!ctx.request.body) {
-      const getRawBody = require('raw-body')
-      const jsonString = await getRawBody(ctx.req)
-      ctx.request.body = JSON.parse(jsonString)
-    }
+async function postActivity (ctx, next) {
+  if (!ctx.request.body) {
+    const getRawBody = require('raw-body')
+    const jsonString = await getRawBody(ctx.req)
+    ctx.request.body = JSON.parse(jsonString)
+  }
 
-    let modelDataList = null
+  let modelDataList = null
 
-    if (Array.isArray(ctx.request.body)) {
-      modelDataList = ctx.request.body
+  if (Array.isArray(ctx.request.body)) {
+    modelDataList = ctx.request.body
+  } else {
+    modelDataList = [ctx.request.body]
+  }
+
+  try {
+    modelDataList.forEach(validate)
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      ctx.status = 422
+      ctx.response.body = {message: error.message}
+      return await next()
     } else {
-      modelDataList = [ctx.request.body]
-    }
-
-    try {
-      modelDataList.forEach(validate)
-    } catch (error) {
-      if (error instanceof ValidationError) {
-        ctx.status = 422
-        ctx.response.body = {message: error.message}
-        return await next()
-      } else {
-        ctx.status = 500
-        return await next()
-      }
-    }
-
-    const create = require('./service/create')(ctx.db)
-
-    modelDataList
-      .map(serialize)
-      .map(create)
-
-    ctx.status = 200
-    await next()
-  },
-  getActivityList: getActivityList,
-  getActivity: getActivity,
-  getMapForActivity: async (ctx, next) => {
-    const getActivity = require('./service/get')(ctx.db)
-    const activity = await getActivity(ctx.params.id)
-    const getMap = require('./service/static-map')(ctx.db)
-    const staticMap = await getMap(activity)
-
-    ctx.body = staticMap.body
-    ctx.type = 'image/png'
-    ctx.status = 200
-    await next()
-  },
-  postActivityTCX: async (ctx, next) => {
-    if (!ctx.request.is('application/vnd.garmin.tcx+xml')) {
+      ctx.status = 500
       return await next()
     }
-
-    const getRawBody = require('raw-body')
-    const xml = await getRawBody(ctx.req)
-
-    try {
-      const activityDtos = require('../../../importer/tcx/tcx-to-dto')(xml)
-      ctx.request.body = activityDtos
-      await next()
-    } catch (error) {
-      ctx.throw(error.message, 500)
-    }
   }
+
+  const create = require('./service/create')(ctx.db)
+
+  modelDataList
+    .map(serialize)
+    .map(create)
+
+  ctx.status = 200
+  await next()
+}
+
+async function postActivityTCX (ctx, next) {
+  if (!ctx.request.is('application/vnd.garmin.tcx+xml')) {
+    return await next()
+  }
+
+  const getRawBody = require('raw-body')
+  const xml = await getRawBody(ctx.req)
+
+  try {
+    const activityDtos = require('../../../importer/tcx/tcx-to-dto')(xml)
+    ctx.request.body = activityDtos
+    await next()
+  } catch (error) {
+    ctx.throw(error.message, 500)
+  }
+}
+
+async function getMapForActivity (ctx, next) {
+  const getActivity = require('./service/get')(ctx.db)
+  const activity = await getActivity(ctx.params.id)
+  const getMap = require('./service/static-map')(ctx.db)
+  const staticMap = await getMap(activity)
+
+  ctx.body = staticMap.body
+  ctx.type = 'image/png'
+  ctx.status = 200
+  await next()
+}
+
+module.exports = {
+  postActivity: postActivity,
+  getActivityList: getActivityList,
+  getActivity: getActivity,
+  getMapForActivity: getMapForActivity,
+  postActivityTCX: postActivityTCX
 }
